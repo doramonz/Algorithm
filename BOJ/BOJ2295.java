d656ddd52b0c635bdb08463cb5fd0ab0bf3598d1
doramonz <whs3867@gmail.com>
2024-10-23 15:18:44 +0900

    BOJ2295

--- BOJ/BOJ2295.java
+++ BOJ/BOJ2295.java
@@ -7,20 +7,21 @@ public class BOJ2295 {
     public static void main(String[] args) throws NumberFormatException, IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        Set<Integer> map = new HashSet<>();
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
         Arrays.sort(arr);
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                map.add(arr[i] + arr[j]);
+            for (int j = i; j < arr.length; j++) {
+                list.add(arr[i] + arr[j]);
             }
         }
+        Collections.sort(list);
         for (int i = arr.length - 1; i >= 0; i--) {
-            for (int j = i; j >= 0; j--) {
-                if (map.contains(arr[i] - arr[j])) {
+            for (int j = 0; j < i; j++) {
+                if (Collections.binarySearch(list, arr[i] - arr[j]) >= 0) {
                     System.out.println(arr[i]);
                     return;
                 }
