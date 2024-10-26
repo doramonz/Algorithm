d273abd337833b95c660b1aa133bff228f4e6bbc
doramonz <whs3867@gmail.com>
2024-10-26 22:52:21 +0900

    BOJ2443

---
+++ BOJ/BOJ2443.java
@@ -0,0 +1,19 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ2443 {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        int n = Integer.parseInt(new BufferedReader(new InputStreamReader(System.in)).readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++)
+                sb.append(" ");
+            for (int j = 0; j < (n - i) * 2 - 1; j++)
+                sb.append("*");
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
