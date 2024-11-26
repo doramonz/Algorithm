a60a692c38c6df8102ca5ef2ca0edb162ebe91a6
doramonz <whs3867@gmail.com>
2024-11-27 01:10:24 +0900

    BOJ3015 improve

--- BOJ/BOJ3015.java
+++ BOJ/BOJ3015.java
@@ -7,31 +7,21 @@ public class BOJ3015 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] arr = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            arr[i][1] = 1;
-            arr[i][0] = Integer.parseInt(br.readLine());
-        }
         Stack<int[]> stack = new Stack<>();
         long cnt = 0;
-        int i = 0;
-        while (i < n) {
-            if (stack.isEmpty()) {
-                stack.add(arr[i]);
-                i++;
-            } else if (stack.peek()[0] == arr[i][0]) {
-                arr[i] = stack.pop();
-                cnt += arr[i][1];
-                arr[i][1]++;
-            } else if (stack.peek()[0] > arr[i][0]) {
-                stack.add(arr[i]);
-                cnt++;
-                i++;
-            } else {
-                cnt += stack.pop()[1];
+        for (int i = 0; i < n; i++) {
+            int v = Integer.parseInt(br.readLine());
+            int h = 1;
+            while (!stack.isEmpty() && stack.peek()[0] <= v) {
+                int[] pop = stack.pop();
+                cnt += pop[1];
+                if (pop[0] == v)
+                    h += pop[1];
             }
+            stack.add(new int[] { v, h });
+            if (!stack.isEmpty() && stack.size() > 1)
+                cnt++;
         }
-
         System.out.println(cnt);
     }
 }
