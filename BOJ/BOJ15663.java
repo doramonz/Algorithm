35ea0b201de6abea694eed72ae46f02ccaf05e13
doramonz <whs3867@gmail.com>
2024-12-11 01:09:54 +0900

    BOJ15663

---
+++ BOJ/BOJ15663.java
@@ -0,0 +1,45 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ15663 {
+    static int n;
+    static int m;
+    static int[] arr;
+    static boolean[] used;
+    static StringBuilder sb = new StringBuilder();
+    static Set<String> set = new HashSet<>();
+
+    static void recur(int count, String s) {
+        if (count == m) {
+            if (set.contains(s))
+                return;
+            set.add(s);
+            sb.append(s).append("\n");
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (used[i])
+                continue;
+            used[i] = true;
+            recur(count + 1, s + arr[i] + " ");
+            used[i] = false;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        used = new boolean[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        recur(0, "");
+        System.out.println(sb);
+    }
+}
