b7198530915a9702c3d7e47ffb3536b680e43f67
doramonz <whs3867@gmail.com>
2024-12-11 01:04:52 +0900

    BOJ15657

---
+++ BOJ/BOJ15657.java
@@ -0,0 +1,35 @@
+package BOJ;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ15657 {
+    static int n;
+    static int m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    static void recur(int count, String s, int index) {
+        if (count == m) {
+            sb.append(s).append("\n");
+            return;
+        }
+        for (int i = index; i < n; i++) {
+            recur(count + 1, s + arr[i] + " ", i);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(st.nextToken());
+        Arrays.sort(arr);
+        recur(0, "",0);
+        System.out.println(sb);
+    }
+}
