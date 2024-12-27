16c59d252a8a69287b8508b4b39552c3dfc7e48f
doramonz <whs3867@gmail.com>
2024-12-28 01:24:32 +0900

    Programmers12913

---
+++ Programmers/Programmers12913.java
@@ -0,0 +1,24 @@
+package Programmers;
+
+public class Programmers12913 {
+    int solution(int[][] land) {
+        int answer = 0;
+        int[][] dp = new int[land.length][4];
+        for(int i = 0;i<4;i++)
+            dp[0][i] = land[0][i];
+        for(int i = 1;i<land.length;i++){
+            for(int j = 0;j<4;j++){
+                int max = 0;
+                for(int k = 0;k<4;k++){
+                    if(j==k)
+                        continue;
+                    max = Math.max(max,dp[i-1][k]);
+                }
+                dp[i][j] = land[i][j] + max;
+            }
+        }
+        for(int i = 0;i<4;i++)
+            answer = Math.max(answer,dp[land.length-1][i]);
+        return answer;
+    }
+}
