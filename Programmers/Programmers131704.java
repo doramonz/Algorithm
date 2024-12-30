e2ea17fe284c9ff016a0c7ce557a5c81b62cd067
doramonz <whs3867@gmail.com>
2024-12-30 12:34:51 +0900

    Programmers131704

---
+++ Programmers/Programmers131704.java
@@ -0,0 +1,22 @@
+package Programmers;
+
+import java.util.Stack;
+
+public class Programmers131704 {
+        public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int v = 1;
+        while(true){
+            if(!stack.isEmpty()&&stack.peek()==order[answer]){
+                stack.pop();
+                answer++;
+            } else if(v<=order.length){
+                stack.push(v++);
+            }else{
+                break;
+            }
+        }
+        return answer;
+    }
+}
