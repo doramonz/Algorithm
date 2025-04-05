0f5a28a8148cf90a27ff302b58fd042563f4bbf9
doramonz <whs3867@gmail.com>
2025-04-05 21:27:05 +0900

    BOJ1149

--- BOJ/BOJ1149.java
+++ BOJ/BOJ1149.java
@@ -1,49 +1,22 @@
 package BOJ;
 
-import java.util.Scanner;
-
+import java.io.*;
+import java.util.*;
 public class BOJ1149 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        int n = scanner.nextInt();
-
-        int[][] d = new int[n][3];
-        int[][] v = new int[n][3];
-
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int arr[][] = new int[n][3];
         for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++)
-                d[i][j] = scanner.nextInt();
+                arr[i][j] = Integer.parseInt(st.nextToken());
         }
-
-        scanner.close();
-
-        v[0][0] = d[0][0];
-        v[0][1] = d[0][1];
-        v[0][2] = d[0][2];
-
         for (int i = 1; i < n; i++) {
-            if (v[i - 1][1] < v[i - 1][2]) {
-                v[i][0] = d[i][0] + v[i - 1][1];
-            } else {
-                v[i][0] = d[i][0] + v[i - 1][2];
-            }
-            if (v[i - 1][0] < v[i - 1][2]) {
-                v[i][1] = d[i][1] + v[i - 1][0];
-            } else {
-                v[i][1] = d[i][1] + v[i - 1][2];
-            }
-            if (v[i - 1][0] < v[i - 1][1]) {
-                v[i][2] = d[i][2] + v[i - 1][0];
-            } else {
-                v[i][2] = d[i][2] + v[i - 1][1];
-            }
+            arr[i][0] += Math.min(arr[i-1][1], arr[i-1][2]);
+            arr[i][1] += Math.min(arr[i-1][0], arr[i-1][2]);
+            arr[i][2] += Math.min(arr[i-1][0], arr[i-1][1]);
         }
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < 3; i++) {
-            if (v[n - 1][i] < min)
-                min = v[n - 1][i];
-        }
-        System.out.println(min);
+        System.out.println(Math.min(arr[n-1][0], Math.min(arr[n-1][1], arr[n-1][2])));
     }
 }
