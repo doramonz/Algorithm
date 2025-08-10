75522bc60677e764a8dbe2ef9f1bd8ed57f5b668
doramonz <whs3867@gmail.com>
2025-08-10 21:47:07 +0900

    ThreeSum

---
+++ LeetCode/3SUM.ts
@@ -0,0 +1,32 @@
+function threeSum(nums: number[]): number[][] {
+    const result: number[][] = [];
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const value = nums[i] + nums[left] + nums[right];
+
+            if (value === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                while (left < right && nums[left] === nums[left + 1]) left++;
+                while (left < right && nums[right] === nums[right - 1]) right--;
+                left++;
+                right--;
+            }else if (value < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+}
+
+console.log(threeSum([-1, 0, 1, 2, -1, -4]));
