c4125310cbd762547d4a3b9d8d9c887568c941de
doramonz <whs3867@gmail.com>
2025-08-11 16:14:37 +0900

    LongestPalindromicSubstring

--- LeetCode/LongestPalindromicSubstring.ts
+++ LeetCode/LongestPalindromicSubstring.ts
@@ -1,29 +1,29 @@
 function longestPalindrome(s: string): string {
     let result = ""
 
-    for(let i = 0;i<s.length;i++){
+    for (let i = 0; i < s.length; i++) {
         let left = i;
         let right = i;
 
-        while(s.charAt(left) === s.charAt(right) && left >= 0 && right < s.length){
+        while (s.charAt(left) === s.charAt(right) && left >= 0 && right < s.length) {
             left--;
             right++;
         }
 
-        if(right - left - 1 > result.length){
+        if (right - left - 1 > result.length) {
             result = s.substring(left + 1, right);
         }
 
-        if(i + 1 < s.length && s.charAt(i) === s.charAt(i + 1)){
+        if (i + 1 < s.length && s.charAt(i) === s.charAt(i + 1)) {
             left = i;
             right = i + 1;
 
-            while(s.charAt(left) === s.charAt(right) && left >= 0 && right < s.length){
+            while (s.charAt(left) === s.charAt(right) && left >= 0 && right < s.length) {
                 left--;
                 right++;
             }
 
-            if(right - left - 1 > result.length){
+            if (right - left - 1 > result.length) {
                 result = s.substring(left + 1, right);
             }
         }
