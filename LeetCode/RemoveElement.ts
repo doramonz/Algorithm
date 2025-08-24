2c7e319ed3eeaa1c644fd75c8ece150a51234382
doramonz <whs3867@gmail.com>
2025-08-24 10:44:14 +0900

    RemovElement

---
+++ LeetCode/RemoveElement.ts
@@ -0,0 +1,11 @@
+function removeElement(nums: number[], val: number): number {
+    for (let i = nums.length - 1; i >= 0; i--) {
+        if(nums[i] === val) {
+            nums.splice(i, 1);
+        }
+    }
+
+    return nums.length;
+};
+
+console.log(removeElement([3,2,2,3], 3)); // 2
